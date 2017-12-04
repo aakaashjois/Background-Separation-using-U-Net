6f5ce8e06b944fe496e0a59294dd1d057f886230
Hextence <aakaashjois@gmail.com>
2017-12-04 08:45:54 -0500

    Rewrite U-net Code

--- lib/generator.py
+++ lib/generator.py
@@ -25,8 +25,17 @@ def image_generator(images_dir, masks_dir, images, masks, batch_size, img_dim = 
             mask = load_img(join(masks_dir, masks[index]), target_size = img_dim, grayscale=True)
             mask_array = img_to_array(mask) / 255
             
-            img_array, mask_array = augmentations.random_augmentation(img_array, mask_array)
+            # img_array, mask_array = augmentations.random_augmentation(img_array, mask_array)
+            img_array, mask_array = augmentations.random_augmentation(img_array,
+                                                                      mask_array,
+                                                                      flip_chance = 0, 
+                                                                      rotate_chance = 0,
+                                                                      shift_chance = 0,
+                                                                      zoom_chance = 0,
+                                                                      shear_chance = 0,
+                                                                      color_quantize = False,
+                                                                      letter_box = True)
             i.append(img_array)
-            m.append(mask_array)
+            m.append(mask_array[:, :, 0])
             
-        yield np.array(i), np.array(m)
+        yield np.array(i), np.array(m).reshape(-1, img_dim[0], img_dim[1], 1)
--- lib/unet.py
+++ lib/unet.py
@@ -1,4 +1,4 @@
-from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Concatenate, Dropout
+from keras.layers import Input, Conv2D, MaxPooling2D, __up__Sampling2D, Concatenate, Dropout
 from keras.models import Model
 from keras.optimizers import Adam
 import keras.backend as K
@@ -11,65 +11,133 @@ def __get_conv_layer_with_dropout__(filter_size, input_layer, dropout, mask_shap
     else:
         return conv2d
 
-def dice_coef(y_true, y_pred):
-    smooth = 1.
-    y_true_f = np.array(y_true).flatten()
-    y_pred_f = np.array(y_pred).flatten()
-    intersection = np.sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
-   
-    # intersect = 2 * y_true * y_pred
-    # len_true = sqrt(sum((y_true)**2))
-    # len_pred = sqrt(sum((y_pred)**2))
-    # loss = intersect/(len_true+len_pred)
-    # return loss
+def dice_coef(y_true, y_pred, smooth=1e-5):
+    y_true = K.round(K.reshape(y_true, [-1]))
+    y_pred = K.round(K.reshape(y_pred, [-1]))
+    isct = K.reduce_sum(y_true * y_pred)
+    return 2 * isct / (K.reduce_sum(y_true) + K.reduce_sum(y_pred))
+
+def __down__(input_layer, filters, pool=True):
+    conv1 = Conv2D(filters, (3, 3), padding='same', activation='relu')(input_layer)
+    residual = Conv2D(filters, (3, 3), padding='same', activation='relu')(conv1)
+    if pool:
+        max_pool = MaxPool2D()(residual)
+        return max_pool, residual
+    else:
+        return residual
+
+def __up__(input_layer, residual, filters):
+    filters=int(filters)
+    __up__sample = __up__Sampling2D()(input_layer)
+    __up__conv = Conv2D(filters, kernel_size=(2, 2), padding="same")(__up__sample)
+    concat = Concatenate(axis=3)([residual, __up__conv])
+    conv1 = Conv2D(filters, (3, 3), padding='same', activation='relu')(concat)
+    conv2 = Conv2D(filters, (3, 3), padding='same', activation='relu')(conv1)
+    return conv2
+
+def get_unet_model(input_shape, filters = 64):
     
+    input_layer = Input(shape = input_shape)
+    residuals = []
+
+    # __down__ 1, 128
+    d1, res1 = __down__(input_layer, filters)
+    residuals.append(res1)
+
+    filters *= 2
+
+    # __down__ 2, 64
+    d2, res2 = __down__(d1, filters)
+    residuals.append(res2)
+
+    filters *= 2
+
+    # __down__ 3, 32
+    d3, res3 = __down__(d2, filters)
+    residuals.append(res3)
+
+    filters *= 2
+
+    # __down__ 4, 16
+    d4, res4 = __down__(d3, filters)
+    residuals.append(res4)
+
+    filters *= 2
+
+    # __down__ 5, 8
+    d5 = __down__(d4, filters, pool=False)
+
+    # __up__ 1, 16
+    u1 = __up__(d5, residual=residuals[-1], filters=filters/2)
+
+    filters /= 2
+
+    # __up__ 2,  32
+    u2 = __up__(u1, residual=residuals[-2], filters=filters/2)
+
+    filters /= 2
+
+    # __up__ 3, 64
+    u3 = __up__(u2, residual=residuals[-3], filters=filters/2)
+
+    filters /= 2
+
+    # __up__ 4, 128
+    u4 = __up__(u3, residual=residuals[-4], filters=filters/2)
+
+    out = Conv2D(filters=1, kernel_size=(1, 1), activation="sigmoid")(__up__4)
+
+    model = Model(input_layer, out)
+    
+    
+'''    
 def get_unet_model(input_shape, learning_rate, dropout=True, loss='binary_crossentropy'):
     # Input layer
     input_layer = Input(shape=input_shape)
-    # Down 1
-    down_conv_1 = __get_conv_layer_with_dropout__(64, input_layer, dropout)
-    residual_1 = __get_conv_layer_with_dropout__(64, down_conv_1, dropout=False)
+    # ____down____ 1
+    ____down_____conv_1 = __get_conv_layer_with_dropout__(64, input_layer, dropout)
+    residual_1 = __get_conv_layer_with_dropout__(64, ____down_____conv_1, dropout=False)
     maxpool_1 = MaxPooling2D()(residual_1)
-    # Down 2
-    down_conv_2 = __get_conv_layer_with_dropout__(128, maxpool_1, dropout)
-    residual_2 = __get_conv_layer_with_dropout__(128, down_conv_2, dropout=False)
+    # __down__ 2
+    __down___conv_2 = __get_conv_layer_with_dropout__(128, maxpool_1, dropout)
+    residual_2 = __get_conv_layer_with_dropout__(128, __down___conv_2, dropout=False)
     maxpool_2 = MaxPooling2D()(residual_2)
-    # Down 3
-    down_conv_3 = __get_conv_layer_with_dropout__(256, maxpool_2, dropout)
-    residual_3 = __get_conv_layer_with_dropout__(256, down_conv_3, dropout=False)
+    # __down__ 3
+    __down___conv_3 = __get_conv_layer_with_dropout__(256, maxpool_2, dropout)
+    residual_3 = __get_conv_layer_with_dropout__(256, __down___conv_3, dropout=False)
     maxpool_3 = MaxPooling2D()(residual_3)
-    # Down 4
-    down_conv_4 = __get_conv_layer_with_dropout__(512, maxpool_3, dropout)
-    residual_4 = __get_conv_layer_with_dropout__(512, down_conv_4, dropout=False)
+    # __down__ 4
+    __down___conv_4 = __get_conv_layer_with_dropout__(512, maxpool_3, dropout)
+    residual_4 = __get_conv_layer_with_dropout__(512, __down___conv_4, dropout=False)
     maxpool_4 = MaxPooling2D()(residual_4)
     # Flat
     flat_conv = __get_conv_layer_with_dropout__(1024, maxpool_4, dropout)
     residual_flat = __get_conv_layer_with_dropout__(1024, flat_conv, dropout=False)
-    # Up 1
-    upsample_1 = UpSampling2D()(residual_flat)
-    concat_1 = Concatenate(axis=3)([residual_4, upsample_1])
-    up_conv_1d = __get_conv_layer_with_dropout__(512, concat_1, dropout)
-    up_conv_1 = __get_conv_layer_with_dropout__(512, up_conv_1d, dropout=False)
-    # Up 2
-    upsample_2 = UpSampling2D()(up_conv_1)
-    concat_2 = Concatenate(axis=3)([residual_3, upsample_2])
-    up_conv_2d = __get_conv_layer_with_dropout__(256, concat_2, dropout)
-    up_conv_2 = __get_conv_layer_with_dropout__(256, up_conv_2d, dropout=False)
-    # Up 3
-    upsample_3 = UpSampling2D()(up_conv_2)
-    concat_3 = Concatenate(axis=3)([residual_2, upsample_3])
-    up_conv_3d = __get_conv_layer_with_dropout__(128, concat_3, dropout)
-    up_conv_3 = __get_conv_layer_with_dropout__(128, up_conv_3d, dropout=False)
-    # Up 4
-    upsample_4 = UpSampling2D()(up_conv_3)
-    concat_4 = Concatenate(axis=3)([residual_1, upsample_4])
-    up_conv_4d = __get_conv_layer_with_dropout__(64, concat_4, dropout)
-    up_conv_4 = __get_conv_layer_with_dropout__(64, up_conv_4d, dropout=False)
+    # __up__ 1
+    __up__sample_1 = __up__Sampling2D()(residual_flat)
+    concat_1 = Concatenate(axis=3)([residual_4, __up__sample_1])
+    __up___conv_1d = __get_conv_layer_with_dropout__(512, concat_1, dropout)
+    __up___conv_1 = __get_conv_layer_with_dropout__(512, __up___conv_1d, dropout=False)
+    # __up__ 2
+    __up__sample_2 = __up__Sampling2D()(__up___conv_1)
+    concat_2 = Concatenate(axis=3)([residual_3, __up__sample_2])
+    __up___conv_2d = __get_conv_layer_with_dropout__(256, concat_2, dropout)
+    __up___conv_2 = __get_conv_layer_with_dropout__(256, __up___conv_2d, dropout=False)
+    # __up__ 3
+    __up__sample_3 = __up__Sampling2D()(__up___conv_2)
+    concat_3 = Concatenate(axis=3)([residual_2, __up__sample_3])
+    __up___conv_3d = __get_conv_layer_with_dropout__(128, concat_3, dropout)
+    __up___conv_3 = __get_conv_layer_with_dropout__(128, __up___conv_3d, dropout=False)
+    # __up__ 4
+    __up__sample_4 = __up__Sampling2D()(__up___conv_3)
+    concat_4 = Concatenate(axis=3)([residual_1, __up__sample_4])
+    __up___conv_4d = __get_conv_layer_with_dropout__(64, concat_4, dropout)
+    __up___conv_4 = __get_conv_layer_with_dropout__(64, __up___conv_4d, dropout=False)
     # Output layer
-    output_layer = out = Conv2D(filters=1, kernel_size=(1, 1), activation="sigmoid")(up_conv_4)
+    output_layer = out = Conv2D(filters=1, kernel_size=(1, 1), activation="sigmoid")(__up___conv_4)
 
     # Create Model
     model = Model(input_layer, output_layer)
     model.compile(optimizer=Adam(learning_rate), loss=loss, metrics=[dice_coef])
     return model
+'''
