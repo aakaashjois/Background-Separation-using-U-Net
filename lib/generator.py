59b8bcb9c942a6d5c026df7c5423353f5e4bacb9
Alp Aygar <aa3250@nyu.edu>
2017-12-05 14:01:46 +0000

    Added augmentations

--- lib/generator.py
+++ lib/generator.py
@@ -20,14 +20,14 @@ def image_generator(images_dir, masks_dir, images, masks, batch_size, img_dim = 
             
             img_array, mask_array = augmentations.random_augmentation(img_array,
                                                                       mask_array,
-                                                                      flip_chance = 0, 
-                                                                      rotate_chance = 0,
-                                                                      shift_chance = 0,
-                                                                      zoom_chance = 0,
-                                                                      shear_chance = 0,
+                                                                      flip_chance = 0.05, 
+                                                                      rotate_chance = 0.05,
+                                                                      shift_chance = 0.05,
+                                                                      zoom_chance = 0.05,
+                                                                      shear_chance = 0.05,
                                                                       color_quantize = False,
                                                                       letter_box = True)
             i.append(img_array)
             m.append(mask_array[:, :, 0])
             
-        yield np.array(i), np.array(m).reshape(-1, img_dim[0], img_dim[1], 1)
+        yield np.array(i), np.array(m).reshape(-1, img_dim[1], img_dim[1], 1)
