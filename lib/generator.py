70544360493c9055192b54e0c476d5fd1960c7d6
Aakaash Jois <aakaashjois@gmail.com>
2017-12-08 13:22:23 -0500

    Add more layers in Unet
    Create code for running all combinations

--- lib/generator.py
+++ lib/generator.py
@@ -20,11 +20,40 @@ def image_generator(images_dir, masks_dir, images, masks, batch_size, img_dim = 
             
             img_array, mask_array = augmentations.random_augmentation(img_array,
                                                                       mask_array,
-                                                                      flip_chance = 0.05, 
-                                                                      rotate_chance = 0.05,
-                                                                      shift_chance = 0.05,
-                                                                      zoom_chance = 0.05,
-                                                                      shear_chance = 0.05,
+                                                                      flip_chance = 0.1, 
+                                                                      rotate_chance = 0.1,
+                                                                      shift_chance = 0.1,
+                                                                      zoom_chance = 0.1,
+                                                                      shear_chance = 0.1,
+                                                                      color_quantize = True,
+                                                                      letter_box = True)
+            i.append(img_array)
+            m.append(mask_array[:, :, 0])
+            
+        yield np.array(i), np.array(m).reshape(-1, img_dim[1], img_dim[1], 1)
+
+def image_generator_no_aug(images_dir, masks_dir, images, masks, batch_size, img_dim = None, num_colors = 256):
+    
+    while True:
+        random_indices = np.random.choice(len(images), batch_size)
+        i = []
+        m = []
+        
+        for index in random_indices:
+            
+            img = load_img(join(images_dir, images[index]), target_size = img_dim)
+            img_array = img_to_array(img) / 255
+            
+            mask = load_img(join(masks_dir, masks[index]), target_size = img_dim, grayscale = True)
+            mask_array = img_to_array(mask) / 255
+            
+            img_array, mask_array = augmentations.random_augmentation(img_array,
+                                                                      mask_array,
+                                                                      flip_chance = 0.0, 
+                                                                      rotate_chance = 0.0,
+                                                                      shift_chance = 0.0,
+                                                                      zoom_chance = 0.0,
+                                                                      shear_chance = 0.0,
                                                                       color_quantize = False,
                                                                       letter_box = True)
             i.append(img_array)
--- lib/unet.py
+++ lib/unet.py
@@ -2,17 +2,7 @@ from keras.layers import Input, Conv2D, MaxPool2D, UpSampling2D, Concatenate, Dr
 from keras.models import Model
 from keras.optimizers import Adam
 import keras.backend as K
-import numpy as np
-
-def __get_conv_layer_with_dropout__(filter_size, input_layer, dropout, mask_shape = (3, 3), dropout_rate = 0.2):
-    conv2d = Conv2D(filter_size, mask_shape, padding = 'same', activation = 'relu')(input_layer)
-    if dropout:
-        return Dropout(dropout_rate)(conv2d)
-    else:
-        return conv2d
-
-def lr_schedule(epoch=1, lr = 1.5e-4):
-    return lr/(epoch*0.1 + 1)    
+import numpy as np   
 
 def dice_coef(y_true, y_pred):
     y_true = K.round(K.reshape(y_true, [-1]))
@@ -38,57 +28,48 @@ def __up__(input_layer, residual, filters):
     conv2 = Conv2D(filters, (3, 3), padding = 'same', activation = 'relu')(conv1)
     return conv2
 
-def get_unet_model(input_shape, filters = 64):
+def get_unet_model(input_shape, filters = 32):
     
     input_layer = Input(shape = input_shape)
     residuals = []
 
-    # __down__ 1, 128
     d1, res1 = __down__(input_layer, filters)
     residuals.append(res1)
 
     filters *= 2
-
-    # __down__ 2, 64
     d2, res2 = __down__(d1, filters)
     residuals.append(res2)
 
     filters *= 2
-
-    # __down__ 3, 32
     d3, res3 = __down__(d2, filters)
     residuals.append(res3)
 
     filters *= 2
-
-    # __down__ 4, 16
     d4, res4 = __down__(d3, filters)
     residuals.append(res4)
 
     filters *= 2
+    d5, res4 = __down__(d3, filters)
+    residuals.append(res4)
 
-    # __down__ 5, 8
-    d5 = __down__(d4, filters, pool=False)
+    filters *= 2
+    d6 = __down__(d4, filters, pool=False)
 
-    # __up__ 1, 16
     u1 = __up__(d5, residual = residuals[-1], filters = filters / 2)
 
     filters /= 2
-
-    # __up__ 2,  32
     u2 = __up__(u1, residual = residuals[-2], filters = filters / 2)
 
     filters /= 2
-
-    # __up__ 3, 64
     u3 = __up__(u2, residual = residuals[-3], filters = filters / 2)
 
     filters /= 2
-
-    # __up__ 4, 128
     u4 = __up__(u3, residual = residuals[-4], filters = filters / 2)
+    
+    filters /= 2
+    u5 = __up__(u3, residual = residuals[-4], filters = filters / 2)
 
-    out = Conv2D(filters = 1, kernel_size = (1, 1), activation = 'hard_sigmoid')(u4)
+    out = Conv2D(filters = 1, kernel_size = (1, 1), activation = 'sigmoid')(u5)
 
     model = Model(input_layer, out)
     return model
