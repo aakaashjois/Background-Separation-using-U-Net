7ed0027d311a1eb686428474381af8ffc11562d0
Aakaash Jois <aakaashjois@gmail.com>
2017-12-12 10:55:00 -0500

    Add more comments

--- modules/UNetModel.py
+++ modules/UNetModel.py
@@ -4,14 +4,19 @@ from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 import keras.backend as K 
 
-def dice_coef(y_true, y_pred):
-    
-    y_true = K.round(K.reshape(y_true, [-1]))
-    y_pred = K.round(K.reshape(y_pred, [-1]))
-    isct = K.sum(y_true * y_pred)
-    return 2 * isct / (K.sum(y_true) + K.sum(y_pred))
 
 def __down__(input_layer, filters, pool = True):
+    """
+    Private method to perform downward propogation in the U-Net architecture.
+    
+    Args:
+        input_layer: Keras Layer. The layer to take as input for the current layer.
+        filters: Integer. The number of channels of output from the current layer.
+        pool: Boolean. Whether to perform Maxpooling on the current layer. (Default: True)
+    
+    Returns:
+        The MaxPooled layer(if pool = True) and the residual layer.
+    """
     conv1 = Conv2D(filters, (3, 3), padding = 'same', activation = 'relu')(input_layer)
     residual = Conv2D(filters, (3, 3), padding = 'same', activation = 'relu')(conv1)
     if pool:
@@ -21,7 +26,17 @@ def __down__(input_layer, filters, pool = True):
         return residual
 
 def __up__(input_layer, residual, filters):
+    """
+    Private method to perform upward propogration in the U-Net architecture.
     
+    Args:
+        input_layer: Keras Layer. The layer to take as input for the current layer.
+        residual: Keras Layer. The residual layer which is taken as input for the current layer.
+        filters: Integer. The number of channels of output from the current layer.
+        
+    Returns:
+        A Keras Layer after UpSampling and Concatenation.
+    """
     filters = int(filters)
     upsample = UpSampling2D()(input_layer)
     upconv = Conv2D(filters, kernel_size = (2, 2), padding = "same")(upsample)
@@ -31,7 +46,16 @@ def __up__(input_layer, residual, filters):
     return conv2
 
 def __create_unet_model__(input_shape, filters = 64):
+    """
+    Private method to create the U-Net model.
     
+    Args:
+        input_shape: List. The shape of the input image
+        filters: Integer. The number of filters to use in the first layer. (Default: 64)
+        
+    Returns:
+        A Keras Model of the U-Net.
+    """
     input_layer = Input(input_shape)
     
     d1, res1 = __down__(input_layer, filters)
@@ -64,7 +88,32 @@ def __create_unet_model__(input_shape, filters = 64):
 
     return Model(input_layer, out)
 
+def dice_coef(y_true, y_pred):
+    """
+    Returns the Dice Coefficient of the two images passed.
+    
+    Args:
+        y_true: Tensor. The original image.
+        y_pred: Tensor. The predicted image.
+    
+    Returns:
+        The Dice Coefficient.
+    """
+    y_true = K.round(K.reshape(y_true, [-1]))
+    y_pred = K.round(K.reshape(y_pred, [-1]))
+    isct = K.sum(y_true * y_pred)
+    return 2 * isct / (K.sum(y_true) + K.sum(y_pred))
+
 def get_unet_model(img_width):
+    """
+    Obtain the compiled U-Net model with the callbacks.
+    
+    Args:
+        img_width: Integer. The width of the input image.
+    
+    Returns:
+        A tuple of compiled U-Net model and the list of callbacks.
+    """
     model = __create_unet_model__([img_width, img_width, 3])
     model.compile(optimizer=Adam(1e-4), 
                   loss='binary_crossentropy',
