a5ddd1d82de06c02c091364133e548ba92eb162f
Hextence <aakaashjois@gmail.com>
2017-12-21 15:35:13 -0500

    Improve code readability
    Make Unet model easier to read

--- modules/UNetModel.py
+++ modules/UNetModel.py
@@ -5,46 +5,6 @@ from keras.callbacks import ModelCheckpoint
 import keras.backend as K 
 
 
-def __down__(input_layer, filters, pool = True):
-    """
-    Performs downward propogation in the U-Net architecture.
-    
-    Args:
-        input_layer: Keras Layer. The layer to take as input for the current layer.
-        filters: Integer. The number of channels of output from the current layer.
-        pool: Boolean. Whether to perform Maxpooling on the current layer. (Default: True)
-    
-    Returns:
-        The MaxPooled layer(if pool = True) and the residual layer.
-    """
-    conv1 = Conv2D(filters, (3, 3), padding = 'same', activation = 'relu')(input_layer)
-    residual = Conv2D(filters, (3, 3), padding = 'same', activation = 'relu')(conv1)
-    if pool:
-        max_pool = MaxPool2D()(residual)
-        return max_pool, residual
-    else:
-        return residual
-
-def __up__(input_layer, residual, filters):
-    """
-    Performs upward propogration in the U-Net architecture.
-    
-    Args:
-        input_layer: Keras Layer. The layer to take as input for the current layer.
-        residual: Keras Layer. The residual layer which is taken as input for the current layer.
-        filters: Integer. The number of channels of output from the current layer.
-        
-    Returns:
-        A Keras Layer after UpSampling and Concatenation.
-    """
-    filters = int(filters)
-    upsample = UpSampling2D()(input_layer)
-    upconv = Conv2D(filters, kernel_size = (2, 2), padding = "same")(upsample)
-    concat = Concatenate(axis = 3)([residual, upconv])
-    conv1 = Conv2D(filters, (3, 3), padding = 'same', activation = 'relu')(concat)
-    conv2 = Conv2D(filters, (3, 3), padding = 'same', activation = 'relu')(conv1)
-    return conv2
-
 def __create_unet_model__(input_shape, filters = 64):
     """
     Creates the U-Net model.
@@ -57,34 +17,63 @@ def __create_unet_model__(input_shape, filters = 64):
         A Keras Model of the U-Net.
     """
     input_layer = Input(input_shape)
-    
-    d1, res1 = __down__(input_layer, filters)
+
+    conv1 = Conv2D(filters, (3, 3), padding='same', activation='relu')(input_layer)
+    residual1 = Conv2D(filters, (3, 3), padding='same', activation='relu')(conv1)
+    max_pool1 = MaxPool2D()(residual1)
     filters *= 2
 
-    d2, res2 = __down__(d1, filters)
+    conv2 = Conv2D(filters, (3, 3), padding='same', activation='relu')(max_pool1)
+    residual2 = Conv2D(filters, (3, 3), padding='same', activation='relu')(conv2)
+    max_pool2 = MaxPool2D()(residual2)
     filters *= 2
 
-    d3, res3 = __down__(d2, filters)
+    conv3 = Conv2D(filters, (3, 3), padding='same', activation='relu')(max_pool2)
+    residual3 = Conv2D(filters, (3, 3), padding='same', activation='relu')(conv3)
+    max_pool3 = MaxPool2D()(residual3)
     filters *= 2
 
-    d4, res4 = __down__(d3, filters)
+    conv4 = Conv2D(filters, (3, 3), padding='same', activation='relu')(max_pool3)
+    residual4 = Conv2D(filters, (3, 3), padding='same', activation='relu')(conv4)
+    max_pool4 = MaxPool2D()(residual4)
     filters *= 2
 
-    d5 = __down__(d4, filters, pool=False)
+    conv5 = Conv2D(filters, (3, 3), padding='same', activation='relu')(max_pool4)
+    residual5 = Conv2D(filters, (3, 3), padding='same', activation='relu')(conv5)
+    filters /= 2
+
+    filters = int(filters)
+    upsample6 = UpSampling2D()(residual5)
+    upconv6 = Conv2D(filters, kernel_size=(2, 2), padding="same")(upsample6)
+    concat6 = Concatenate(axis=3)([residual4, upconv6])
+    conv6 = Conv2D(filters, (3, 3), padding='same', activation='relu')(concat6)
+    conv7 = Conv2D(filters, (3, 3), padding='same', activation='relu')(conv6)
 
     filters /= 2
-    u1 = __up__(d5, residual = res4, filters = filters)
+    filters = int(filters)
+    upsample7 = UpSampling2D()(conv7)
+    upconv7 = Conv2D(filters, kernel_size=(2, 2), padding="same")(upsample7)
+    concat7 = Concatenate(axis=3)([residual3, upconv7])
+    conv8 = Conv2D(filters, (3, 3), padding='same', activation='relu')(concat7)
+    conv9 = Conv2D(filters, (3, 3), padding='same', activation='relu')(conv8)
 
     filters /= 2
-    u2 = __up__(u1, residual = res3, filters = filters)
+    filters = int(filters)
+    upsample8 = UpSampling2D()(conv9)
+    upconv8 = Conv2D(filters, kernel_size=(2, 2), padding="same")(upsample8)
+    concat8 = Concatenate(axis=3)([residual2, upconv8])
+    conv10 = Conv2D(filters, (3, 3), padding='same', activation='relu')(concat8)
+    conv11 = Conv2D(filters, (3, 3), padding='same', activation='relu')(conv10)
 
     filters /= 2
-    u3 = __up__(u2, residual = res2, filters = filters)
+    filters = int(filters)
+    upsample9 = UpSampling2D()(conv11)
+    upconv9 = Conv2D(filters, kernel_size=(2, 2), padding="same")(upsample9)
+    concat9 = Concatenate(axis=3)([residual1, upconv9])
+    conv12 = Conv2D(filters, (3, 3), padding='same', activation='relu')(concat9)
+    conv13 = Conv2D(filters, (3, 3), padding='same', activation='relu')(conv12)
 
-    filters /= 2
-    u4 = __up__(u3, residual = res1, filters = filters)
-
-    out = Conv2D(filters = 1, kernel_size = (1, 1), activation = 'sigmoid')(u4)
+    out = Conv2D(filters = 1, kernel_size = (1, 1), activation = 'sigmoid')(conv13)
 
     return Model(input_layer, out)
 
